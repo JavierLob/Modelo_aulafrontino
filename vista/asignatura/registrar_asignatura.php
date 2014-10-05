64d697c6a6a4ed1744e27d9e625b3808ca903bf5
Javier Martín <recupera.javier@gmail.com>
2014-10-05 15:24:45 -0430

    migración curso y corrección de identación
    migración curso y corrección de identación

--- vista/asignatura/registrar_asignatura.php
+++ vista/asignatura/registrar_asignatura.php
@@ -1,92 +1,92 @@
-    <h1 class="page-header">Registrar Asignatura</h1>
-    <!-- EMPIEZA: RECOMENDACION -->
-      <div class="alert alert-info" role="alert">
-        <strong><i class="fa fa-info-circle"></i></strong> Aquí podras registrar un Asignatura en el sistema.
+<h1 class="page-header">Registrar Asignatura</h1>
+<!-- EMPIEZA: RECOMENDACION -->
+  <div class="alert alert-info" role="alert">
+    <strong><i class="fa fa-info-circle"></i></strong> Aquí podras registrar un Asignatura en el sistema.
+  </div>
+<!-- FIN: RECOMENDACION -->
+<form role="form" class="form" action="../controlador/control_asignatura.php" method="POST" name="form_asignatura">
+    <input type="hidden" value="registrar_asignatura" name="operacion" />
+    <input type="hidden"  name="idasignatura" id="cam_idasignatura"/>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_codigoasi">Código <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Código de la asignatura."></i></strong></label>
+              <input type="text" name="codigoasi" class="form-control" id="cam_codigoasi" required>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_nombreasi">Nombre <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Nombre de la asignatura."></i></strong></label>
+              <input type="text" name="nombreasi" class="form-control" id="cam_nombreasi" required>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_descripcionasi">Descripción <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Descripción de la asignatura."></i></strong></label>
+              <input type="text" name="descripcionasi" class="form-control" id="cam_descripcionasi" required>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_anoasi">Año <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Año en el cual se cursa la asignatura."></i></strong></label>
+              <select type="text" name="anoasi" class="form-control" id="cam_anoasi" required>
+                <option value="">Elegir</option>
+                <?php
+                    for($i=1;$i<=6;$i++)
+                    {
+                        echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
+                    }
+                ?>
+            </select>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_descripcionasi">Unidad de crédito <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Cantidad de Unidad de crédito de la asignatura."></i></strong></label>
+              <select type="text" name="unidad_creditoasi" class="form-control" id="cam_unidad_creditoasi" required>
+                <option value="">Elegir</option>
+                <?php
+                    for($i=1;$i<=8;$i++)
+                    {
+                        echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
+                    }
+                ?>
+            </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_horas_duracionasi">Horas <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Cantidad de horas de la asignatura."></i></strong></label>
+              <select type="text" name="horas_duracionasi" class="form-control" id="cam_horas_duracionasi" required>
+                <option value="">Elegir</option>
+                <?php
+                    for($i=1;$i<=6;$i++)
+                    {
+                        echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
+                    }
+                ?>
+            </select>
+            </div>
+        </div>
+    </div>
+       <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="cam_descripcionasi">Observación <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Observación con respecto a la asignatura."></i></strong></label>
+                <textarea name="observacionasi" class="form-control" id="cam_observacionasi"></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <button type="button" class="btn btn-danger center-block" name="btn_regresar" id="btn_regresar" onclick="window.location.href='?vista=asignatura/asignatura';"><i class="fa fa-chevron-left"></i> Regresar</button>
       </div>
-    <!-- FIN: RECOMENDACION -->
-    <form role="form" class="form" action="../controlador/control_asignatura.php" method="POST" name="form_asignatura">
-        <input type="hidden" value="registrar_asignatura" name="operacion" />
-        <input type="hidden"  name="idasignatura" id="cam_idasignatura"/>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_codigoasi">Código <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Código de la asignatura."></i></strong></label>
-                  <input type="text" name="codigoasi" class="form-control" id="cam_codigoasi" required>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_nombreasi">Nombre <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Nombre de la asignatura."></i></strong></label>
-                  <input type="text" name="nombreasi" class="form-control" id="cam_nombreasi" required>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_descripcionasi">Descripción <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Descripción de la asignatura."></i></strong></label>
-                  <input type="text" name="descripcionasi" class="form-control" id="cam_descripcionasi" required>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_anoasi">Año <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Año en el cual se cursa la asignatura."></i></strong></label>
-                  <select type="text" name="anoasi" class="form-control" id="cam_anoasi" required>
-                    <option value="">Elegir</option>
-                    <?php
-                        for($i=1;$i<=6;$i++)
-                        {
-                            echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
-                        }
-                    ?>
-                </select>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_descripcionasi">Unidad de crédito <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Cantidad de Unidad de crédito de la asignatura."></i></strong></label>
-                  <select type="text" name="unidad_creditoasi" class="form-control" id="cam_unidad_creditoasi" required>
-                    <option value="">Elegir</option>
-                    <?php
-                        for($i=1;$i<=8;$i++)
-                        {
-                            echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
-                        }
-                    ?>
-                </select>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label for="cam_horas_duracionasi">Horas <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Cantidad de horas de la asignatura."></i></strong></label>
-                  <select type="text" name="horas_duracionasi" class="form-control" id="cam_horas_duracionasi" required>
-                    <option value="">Elegir</option>
-                    <?php
-                        for($i=1;$i<=6;$i++)
-                        {
-                            echo '<option value="'.$i.'" '.$selected.'>'.$i.'</option>';
-                        }
-                    ?>
-                </select>
-                </div>
-            </div>
-        </div>
-           <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="cam_descripcionasi">Observación <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Observación con respecto a la asignatura."></i></strong></label>
-                    <textarea name="observacionasi" class="form-control" id="cam_observacionasi"></textarea>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <button type="button" class="btn btn-danger center-block" name="btn_regresar" id="btn_regresar" onclick="window.location.href='?vista=asignatura/asignatura';"><i class="fa fa-chevron-left"></i> Regresar</button>
-          </div>
-          <div class="col-md-6">
-            <button type="submit" class="btn btn-danger center-block"><i class="fa fa-check" name="btn_enviar" id="btn_enviar"></i> Aceptar</button>
-          </div>
-        </div>
-    </form>
+      <div class="col-md-6">
+        <button type="submit" class="btn btn-danger center-block"><i class="fa fa-check" name="btn_enviar" id="btn_enviar"></i> Aceptar</button>
+      </div>
+    </div>
+</form>
--- vista/curso/consultar_curso.php
+++ vista/curso/consultar_curso.php
@@ -7,42 +7,54 @@
     $lobjCurso->set_Curso($id);
     $laCurso=$lobjCurso->consultar_curso();
 ?>
-<div style="float: left" class="col-lg-8 span8 pull-left">
-    <h3>Consultar Curso</h3>
-    <div class="alert alert-info" role="alert"> <i class="fa fa-info-circle"></i> Aquí podras registrar un Curso en el sistema.</div>
-    <form class="formulario" action="../controlador/control_curso.php" method="POST" name="form_curso">
-        <input type="hidden" value="editar_curso" name="operacion" />
-        <input type="hidden"  name="idcurso" id="cam_idcurso" value="<?php echo $laCurso['idcurso'];?>"/>
-        <div class="row-fluid">
-            <div class="col-lg-6 span6">
-                <label>Nombre <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Nombre del curso."><i class="fa fa-question" ></i></span></label>
-                <input type="text" name="nombrecur" id="cam_nombrecur" value="<?php echo $laCurso['nombrecur'];?>" required/>
-            </div>
-            <div class="col-lg-6 span6">            
-                <label>Sección <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Sección del curso."><i class="fa fa-question" ></i></span></label>
-                <input type="text" name="seccioncur" id="cam_seccioncur" value="<?php echo $laCurso['seccioncur'];?>" required/>
-            </div>
-        </div>
-        <div class="row-fluid">
-            <div class="col-lg-6 span6">            
-                <label>Fecha de apertura <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Fecha de apertura del curso."><i class="fa fa-question" ></i></span></label>
-                <div class="input-append date" data-date="" id="dp2" data-date-format="dd-mm-yyyy">
-                  <input class="span12" size="16" name="fecha_aperturacur" id="cam_fecha_aperturacur" value="<?php echo $laCurso['fecha_aperturacur'];?>" type="text" required>
-                  <span class="add-on"><i class="icon-th"></i></span>
-                </div>
-            </div>
-            <div class="col-lg-6 span6">            
-                <label>Fecha de cierrre <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Fecha de cierrre del curso."><i class="fa fa-question" ></i></span></label>
-                <div class="input-append date" data-date="" id="dp1" data-date-format="dd-mm-yyyy">
-                  <input class="span12" size="16" name="fecha_cierrecur" id="cam_fecha_cierrecur" value="<?php echo $laCurso['fecha_cierrecur'];?>" type="text" required>
-                  <span class="add-on"><i class="icon-th"></i></span>
-                </div>
+<h1 class="page-header">Consultar Curso</h1>
+<!-- EMPIEZA: RECOMENDACION -->
+  <div class="alert alert-info" role="alert">
+    <strong><i class="fa fa-info-circle"></i></strong> Aquí podras registrar un Curso en el sistema.
+  </div>
+<!-- FIN: RECOMENDACION -->
+<form role="form" class="form" action="../controlador/control_curso.php" method="POST" name="form_curso">
+    <input type="hidden" value="editar_curso" name="operacion" />
+    <input type="hidden"  name="idcurso" id="cam_idcurso" value="<?php echo $laCurso['idcurso'];?>"/>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_nombrecur">Nombre <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Nombre del curso."></i></strong></label>
+              <input type="text" name="nombrecur" class="form-control" id="cam_nombrecur" value="<?php echo $laCurso['nombrecur'];?>" required>
             </div>
         </div>
-        <div class="row-fluid">
-            <div class="col-lg-6 span6">            
-                <label>Cupos disponibles <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Cantidad de cupos disponibles para el curso."><i class="fa fa-question" ></i></span></label>
-                <select type="text" name="cupos_disponiblecur" id="cam_cupos_disponiblecur" required>
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_seccioncur">Sección <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Sección del curso."></i></strong></label>
+              <input type="text" name="seccioncur" class="form-control" id="cam_seccioncur" value="<?php echo $laCurso['seccioncur'];?>" required>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_fecha_aperturacur">Fecha de apertura <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Fecha de apertura del curso."></i></strong></label>
+                  <div class="input-group input-append date" data-date="" id="dp1" data-date-format="dd-mm-yyyy">
+                      <span class="input-group-addon add-on"><i class="fa fa-calendar"></i></span>
+                      <input class="form-control" name="fecha_aperturacur" id="cam_fecha_aperturacur" value="<?php echo $laCurso['fecha_aperturacur'];?>" type="text" required>
+                    </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="cam_fecha_cierrecur">Fecha de cierrre <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Fecha de cierrre del curso."></i></strong></label>
+                  <div class="input-group date" data-date="" id="dp2" data-date-format="dd-mm-yyyy">
+                      <span class="input-group-addon add-on"><i class="fa fa-calendar"></i></span>
+                      <input class="form-control" name="fecha_cierrecur" id="cam_fecha_cierrecur" value="<?php echo $laCurso['fecha_cierrecur'];?>" type="text" required>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">            
+            <div class="form-group">
+                <label for="cam_cupos_disponiblecur">Cupos disponibles <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Cantidad de cupos disponibles para el curso."></i></strong></label>
+                <select class="form-control" name="cupos_disponiblecur" id="cam_cupos_disponiblecur" required>
                     <option value="">Elegir</option>
                     <?php
                         for($i=1;$i<=50;$i++)
@@ -53,9 +65,11 @@
                     ?>
                 </select>
             </div>
-            <div class="col-lg-6 span6">            
-                <label>Asignatura <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Asignatura para el curso."><i class="fa fa-question" ></i></span></label>
-                <select type="text" name="idasignatura" id="cam_idasignatura" required>
+        </div>
+        <div class="col-md-6">            
+            <div class="form-group">
+                <label for="cam_idasignatura">Asignatura <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Asignatura para el curso."></i></strong></label>        
+                <select class="form-control" name="idasignatura" id="cam_idasignatura" required>
                     <option value="">Elegir</option>
                     <?php
                         require_once('../clases/clase_asignatura.php');
@@ -69,31 +83,38 @@
                     ?>
                 </select>
             </div>
-        </div>   
-        <div class="row-fluid">
-            <div class="col-lg-6 span6">            
-                <label>Profesor <span class="label label-warning" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="right" data-content="Profesor que dictará el curso."><i class="fa fa-question" ></i></span></label>
-                <select type="text" name="idprofesor" id="cam_idprofesor" required>
+        </div>
+    </div>   
+    <div class="row">
+        <div class="col-md-6">            
+            <div class="form-group">
+                <label for="cam_idprofesor">Profesor <strong><i class="text-help fa fa-question-circle" data-toggle="popover" data-placement="right" data-trigger="hover" data-content="Profesor que dictará el curso."></i></strong></label>
+                <select class="form-control" name="idprofesor" id="cam_idprofesor" required>
                     <option value="">Elegir</option>
                      <?php
                         $laProfesores=$lobjAsignatura->listar_profesores_activos();
                         for($i=0;$i<count($laProfesores);$i++)
                         {
                             $selected=($laProfesores[$i]['idprofesor']==$laCurso['idprofesor'])?'selected':'';
-
                             echo '<option value="'.$laProfesores[$i]['idprofesor'].'" '.$selected.'>'.$laProfesores[$i]['cedulapro'].' - '.$laProfesores[$i]['nombre_unopro'].' '.$laProfesores[$i]['apellido_unopro'].'</option>';
                         }
                     ?>
                 </select>
             </div>
         </div>
-        <div class="botonera">
-            <input type="submit" class="btn btn-success" name="btn_enviar" id="btn_enviar" value="ACEPTAR">
-            <input type="button" class="btn btn-danger" name="btn_regresar" id="btn_regresar" value="REGRESAR" onclick="window.location.href='?vista=curso/curso';">
-        </div>
-    </form>
-</div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <button type="button" class="btn btn-danger center-block" name="btn_regresar" id="btn_regresar" onclick="window.location.href='?vista=curso/curso';"><i class="fa fa-chevron-left"></i> Regresar</button>
+      </div>
+      <div class="col-md-6">
+        <button type="submit" class="btn btn-danger center-block"><i class="fa fa-check" name="btn_enviar" id="btn_enviar"></i> Aceptar</button>
+      </div>
+    </div>
+</form>
 <script>
+$(document).ready(function() {
     $('#dp1').datepicker();
     $('#dp2').datepicker();
+});
 </script>
