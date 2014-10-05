c65f47e8a82d2928eec6aca6ac9fa0adb733c51a
BonceSkip <a1000kr@hotmail.com>
2014-10-05 00:01:26 -0430

    Nucleo
    Nucleo con el comentario necesario.

--- nucleo/ModeloConect.php
+++ nucleo/ModeloConect.php
@@ -1,10 +1,18 @@
 <?php
+/**
+* Nucleo = Core ; Permite conectarme a la base de datos MySQL.
+*
+* @package    ModeloAulafrontino
+* @license    http://www.gnu.org/licenses/gpl.txt  GNU GPL 3.0
+* @author     Equipo de desarrollo Aula Frontino <aulafrontino@gmail.com>
+* @link       https://github.com/EquipoAulaFrontino
+*/
 class ModeloConect
 {
-	private $servidor='Localhost';
-	private $usuario='root';
-	private $clave='';
-	private $bd='bd_sistema_inscripcion';
+	private $servidor = 'Localhost';
+	private $usuario = 'root';
+	private $clave = '';
+	private $bd = 'bd_sistema_inscripcion';
 	protected $laRow = array();
 	private $con;
 	private $res;
@@ -22,71 +30,71 @@ class ModeloConect
 	}
 
 	  
-	  //FUNCION PROTEGIDA FILTRO DE BUSQUEDA
-      protected function filtro($sql)
-      {
-      	 $lrTb=mysql_query($sql,$this->con) OR die(mysql_error());
-      	 return $lrTb;
-      }
+	//FUNCION PROTEGIDA FILTRO DE BUSQUEDA
+	protected function filtro($sql)
+	{
+	  	$lrTb=mysql_query($sql,$this->con) OR die(mysql_error());
+	  	return $lrTb;
+	}
 	  
-      //FUNCION PROTEGIDA CIERRAFILTRO DE BUSQUEDA
-      protected function cierrafiltro($result)
-      {
-         mysql_free_result($result);
-      }
+    //FUNCION PROTEGIDA CIERRAFILTRO DE BUSQUEDA
+    protected function cierrafiltro($result)
+    {
+       mysql_free_result($result);
+    }
 
 	protected function ejecutar($sql)
 	{
 		$this->res=mysql_query($sql,$this->con) or die(mysql_error());
 		if (mysql_affected_rows($this->con)==0)
 		    return false;
-		 return true;
+		return true;
 	}
 
 	//FUNCION PROXIMO PARA RECORRER result PROXIMA POSICION DEL ARRAY
-      protected function proximo($result)
-      {
-      	 $laRow=mysql_fetch_array($result);
-      	 return $laRow;
-      }
+    protected function proximo($result)
+    {
+      	$laRow=mysql_fetch_array($result);
+      	return $laRow;
+    }
       
-	  //FUNCION PROTEGIDA NUMERO DE REGISTROS PARA SABER LA CANTIDAD DE REGISTROS EXISTENTES
-      protected function num_registros($result)
-      {  
- 	     $lnRegistros=mysql_num_rows($result);
- 	     return $lnRegistros;
-      }
+	//FUNCION PROTEGIDA NUMERO DE REGISTROS PARA SABER LA CANTIDAD DE REGISTROS EXISTENTES
+    protected function num_registros($result)
+    {  
+ 	    $lnRegistros=mysql_num_rows($result);
+ 	    return $lnRegistros;
+    }
 
 	//FUNCION BEGIN
-      protected function begin()
-	  {
-	     mysql_query("BEGIN",$this->con);
-	  }
+    protected function begin()
+	{
+	    mysql_query("BEGIN",$this->con);
+	}
 	  
-	  //FUNCION COMMIT
-	  protected function commit()
-	  {
-	     mysql_query("COMMIT",$this->con);
-	  }
+	//FUNCION COMMIT
+	protected function commit()
+	{
+	    mysql_query("COMMIT",$this->con);
+	}
 	  
-	  //FUNCION ROLLBACK
-	  protected function rollback()
-	  {
-	     mysql_query("ROLLBACK",$this->con);
-	  }
+	//FUNCION ROLLBACK
+	protected function rollback()
+	{
+	    mysql_query("ROLLBACK",$this->con);
+	}
 
-	  protected function formato_fecha($pcFecha)
-	  {
-	  	 $lcNow=date("Y-m-d");
-	  	 if (strlen($pcFecha)==10)
-	  	 {
+	protected function formato_fecha($pcFecha)
+	{
+	  	$lcNow=date("Y-m-d");
+	  	if (strlen($pcFecha)==10)
+	  	{
 	  	 	$lcDia=substr($pcFecha,0,2);
 	  	 	$lcMes=substr($pcFecha,3,2);
 	  	 	$lcAno=substr($pcFecha,6,4);
 	  	 	$lcNow=$lcAno."-".$lcMes."-".$lcDia;
-	  	 }
-	  	 return $lcNow;
-	  }
+	  	}
+	  	return $lcNow;
+	}
 
 }
 ?>
